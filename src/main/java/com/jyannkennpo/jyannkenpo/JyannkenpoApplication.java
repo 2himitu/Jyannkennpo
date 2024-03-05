271ff534c4b70f0a693274b6791cef156c121c9f
2himitu <hmhnes@gamil.com>
2024-03-05 16:28:41 +0900

    세팅

---
+++ src/main/java/com/jyannkennpo/jyannkenpo/JyannkenpoApplication.java
@@ -0,0 +1,13 @@
+package com.jyannkennpo.jyannkenpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyannkenpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JyannkenpoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyannkennpo/jyannkenpo/JyannkenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyannkennpo.jyannkenpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyannkenpoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
